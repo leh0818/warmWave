aa3ee8cbef7686b42f3570bafc6a03f82a8d1324
yunhuiGwak <kwakyh17@naver.com>
2023-12-01 00:26:13 +0900

    feat: RequestInstitutionJoinDto 이메일인증여부 추가 RequestInstitutionJoinDto.java - 이메일 인증여부와 어노테이션 수정

--- BE/src/main/java/com/myapp/warmwave/domain/user/dto/RequestInstitutionJoinDto.java
+++ BE/src/main/java/com/myapp/warmwave/domain/user/dto/RequestInstitutionJoinDto.java
@@ -4,23 +4,28 @@ import com.myapp.warmwave.common.Role;
 import com.myapp.warmwave.domain.address.entity.Address;
 import com.myapp.warmwave.domain.user.entity.Institution;
 import com.myapp.warmwave.domain.user.service.UserService;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Getter
+@Data
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
 public class RequestInstitutionJoinDto {
+
+    @NotBlank(message = "email은 필수 입력입니다.")
+    @Email
     private String email;
 
+    @NotBlank(message = "password는 필수 입력입니다.")
     private String password;
 
+    @NotBlank(message = "기관이름은 필수 입력입니다.")
     private String institutionName;
 
+    @NotBlank(message = "사업자 등록번호는 필수 입력입니다.")
     private String registerNum;
 
     // 전체 주소
@@ -45,6 +50,7 @@ public class RequestInstitutionJoinDto {
                 .temperature(0F)
                 .profileImg(UserService.DEFAULT_PROFILE_IMG_INST)
                 .role(Role.INSTITUTION)
+                .emailAuth(false)   // 이메일 인증여부 추가
                 .isApprove(Boolean.FALSE)
                 .build();
     }
