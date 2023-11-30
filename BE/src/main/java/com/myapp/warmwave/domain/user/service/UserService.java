2616486bce4fcb8c3125d55ccf2140c718b1ba8e
yunhuiGwak <kwakyh17@naver.com>
2023-11-30 10:49:16 +0900

    feat: UserService 개인회원(CRU)추가 UserService.java - 개인회원 가입 - 개인회원 정보수정 - 개인회원 전체조회 - 개인회원 단일조회

--- BE/src/main/java/com/myapp/warmwave/domain/user/service/UserService.java
+++ BE/src/main/java/com/myapp/warmwave/domain/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.myapp.warmwave.domain.user.service;
 
+import com.myapp.warmwave.common.Role;
 import com.myapp.warmwave.domain.address.entity.Address;
 import com.myapp.warmwave.domain.address.service.AddressService;
-import com.myapp.warmwave.domain.user.dto.RequestInstitutionJoinDto;
-import com.myapp.warmwave.domain.user.dto.RequestInstitutionUpdateDto;
-import com.myapp.warmwave.domain.user.dto.ResponseUserDto;
+import com.myapp.warmwave.domain.user.dto.*;
+import com.myapp.warmwave.domain.user.entity.Individual;
 import com.myapp.warmwave.domain.user.entity.Institution;
 import com.myapp.warmwave.domain.user.entity.User;
+import com.myapp.warmwave.domain.user.repository.IndividualRepository;
 import com.myapp.warmwave.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,10 +22,12 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class UserService {
     private final UserRepository<User> userRepository;
+    private final IndividualRepository individualRepository;
     private final AddressService addressService;
     private final PasswordEncoder passwordEncoder;
 
     public static final String DEFAULT_PROFILE_IMG_INST = "/static/profile/default_inst.jpg";
+
 
     // 기관 회원가입
     @Transactional
@@ -42,6 +45,23 @@ public class UserService {
         return userRepository.save(institution).getId();
     }
 
+    // 개인 회원가입
+    @Transactional
+    public Long joinIndividual(RequestIndividualJoinDto dto) {
+        if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new IllegalArgumentException(("이미 존재하는 회원"));
+        }
+
+        Individual individual = Individual.builder()
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .nickname(dto.getNickname())
+                .address(addressService.createAddress(dto.getFullAddr(),dto.getSdName(),dto.getSggName(), dto.getDetails()))
+                .role(Role.INDIVIDUAL)
+                .build();
+        return userRepository.save(individual).getId();
+    }
+
     // 승인하지 않은 기관 회원 조회
     public List<ResponseUserDto> findAllByIsApproveFalse() {
         return userRepository.findAllByIsApproveFalse()
@@ -49,28 +69,44 @@ public class UserService {
                 .map(ResponseUserDto::FromEntity)
                 .toList();
     }
-
     // 승인한 기관 회원 조회
+
     public List<ResponseUserDto> findAllByIsApproveTrue() {
         return userRepository.findAllByIsApproveTrue()
                 .stream()
                 .map(ResponseUserDto::FromEntity)
                 .toList();
     }
-
     // 기관 단일 조회 -> 승인한 기관만 조회 가능
+
     public ResponseUserDto findInstitution(Long userId) {
         return userRepository.findByIdAndIsApproveTrue(userId)
                 .map(ResponseUserDto::FromEntity)
                 .orElseThrow(() -> new IllegalArgumentException("에러"));
     }
 
+    // 개인 회원 단일 조회
+    public ResponseUserDto findIndividual(Long userId) {
+        return individualRepository.findById(userId)
+                .map(ResponseUserDto::FromEntity)
+                .orElseThrow(() -> new IllegalArgumentException("에러"));
+    }
     // 전체 기관 회원 조회
+
     public List<ResponseUserDto> findAllByRoleInstitution() {
         return userRepository.findAll()
                 .stream()
                 .map(Institution.class::cast)
 //                .filter(Institution::getIsApprove) -> 승인 여부에 따라 다른데 우선 전체로 기준 잡고 조회함.
+                .map(ResponseUserDto::FromEntity)
+                .toList();
+    }
+
+    // 전체 개인 회원 조회
+    public List<ResponseUserDto> findAllByRoleIndividual() {
+        return individualRepository.findAll()
+                .stream()
+                .map(Individual.class::cast)
                 .map(ResponseUserDto::FromEntity)
                 .toList();
     }
@@ -91,7 +127,23 @@ public class UserService {
         return userRepository.save(savedInstitution).getId();
     }
 
+    // 개인 회원 정보 수정
+    @Transactional
+    public Long updateIndiInfo(RequestIndividualUpdateDto dto, Long userId) {
+
+        Individual savedIndividual = userRepository.findById(userId)
+                .map(Individual.class::cast)
+                .orElseThrow(() -> new IllegalArgumentException("에러"));
+
+        Address address = addressService.updateIndividualAddress(dto, savedIndividual);
+
+        savedIndividual.updateIndiInfo(dto.getNickname(), passwordEncoder.encode(dto.getPassword()), address);
+
+        return userRepository.save(savedIndividual).getId();
+    }
+
     // 기관 가입 승인
+
     @Transactional
     public void changeStatus(Long userId) {
         Institution savedUser = userRepository.findById(userId)
@@ -101,8 +153,8 @@ public class UserService {
         savedUser.approve();
         userRepository.save(savedUser);
     }
-
     // 회원 탈퇴
+
     @Transactional
     public void deleteUser(Long userId) {
         User savedUser = userRepository.findById(userId)
