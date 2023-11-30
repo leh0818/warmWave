b07bf7e7bb4f536c02ad5c17b9b407fca5a4cdab
yunhuiGwak <kwakyh17@naver.com>
2023-11-30 10:53:36 +0900

    feat: UserController 개인회원 CRU 추가 UserController.java - 개인회원가입 - 개인회원 정보수정 - 개인회원 전체조회 - 개인회원 단일조회

--- BE/src/main/java/com/myapp/warmwave/domain/user/controller/UserController.java
+++ BE/src/main/java/com/myapp/warmwave/domain/user/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.myapp.warmwave.domain.user.controller;
 
-import com.myapp.warmwave.domain.user.dto.RequestInstitutionJoinDto;
-import com.myapp.warmwave.domain.user.dto.RequestInstitutionUpdateDto;
-import com.myapp.warmwave.domain.user.dto.ResponseUserDto;
+import com.myapp.warmwave.domain.user.dto.*;
 import com.myapp.warmwave.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,19 +14,32 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
     private final UserService userService;
 
-    // 회원가입
+    // 기관회원가입
     @PostMapping("/register/institution")
     public ResponseEntity<Long> register(@RequestBody RequestInstitutionJoinDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.joinInstitution(dto));
+    }
+
+    // 개인회원가입
+    @PostMapping("/register/individual")
+    public ResponseEntity<Long> signup(@Valid @RequestBody RequestIndividualJoinDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.joinIndividual(dto));
     }
 
     // 전체 기관 회원 조회
     @GetMapping("/institution")
     public ResponseEntity<List<ResponseUserDto>> findAllInstitution() {
         return ResponseEntity.ok(userService.findAllByRoleInstitution());
+    }
+
+    // 전체 개인 회원 조회
+    @GetMapping("/individual")
+    public ResponseEntity<List<ResponseUserDto>> findAllIndividual() {
+        return ResponseEntity.ok(userService.findAllByRoleIndividual());
     }
 
     // 승인된 기관 리스트 조회
@@ -47,6 +60,12 @@ public class UserController {
         return ResponseEntity.ok(userService.findInstitution(userId));
     }
 
+    // 개인 회원 단일 조회
+    @GetMapping("/{userId}/individual")
+    public ResponseEntity<ResponseUserDto> findIndividual(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(userService.findIndividual(userId));
+    }
+
     // 기관 회원 정보 수정
     @PutMapping("/{userId}/institution")
     public ResponseEntity<Long> updateUserInfo(
@@ -54,6 +73,15 @@ public class UserController {
             @RequestBody RequestInstitutionUpdateDto dto
     ) {
         return ResponseEntity.ok(userService.updateInfo(dto, userId));
+    }
+
+    // 개인 회원 정보 수정
+    @PutMapping("/{userId}/individual")
+    public ResponseEntity<Long> updateUserInfo(
+            @PathVariable("userId") Long userId,
+            @RequestBody RequestIndividualUpdateDto dto
+    ) {
+        return ResponseEntity.ok(userService.updateIndiInfo(dto, userId));
     }
 
     // 기관 가입 승인
