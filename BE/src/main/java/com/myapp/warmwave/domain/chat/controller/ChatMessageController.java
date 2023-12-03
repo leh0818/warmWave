0b3bac73d54edd045b40e726531c25c11846c518
yesrin <asjs11@naver.com>
2023-12-03 18:33:21 +0900

    fix : cors 추가 및 message 서비스 로직 수정

--- BE/src/main/java/com/myapp/warmwave/domain/chat/controller/ChatMessageController.java
+++ BE/src/main/java/com/myapp/warmwave/domain/chat/controller/ChatMessageController.java
@@ -1,25 +1,35 @@
 package com.myapp.warmwave.domain.chat.controller;
 
 import com.myapp.warmwave.domain.chat.dto.ChatMessageDto;
+import com.myapp.warmwave.domain.chat.dto.ResponseChatMessageDto;
 import com.myapp.warmwave.domain.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ChatMessageController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;  //특정 Broker로 메세지 전달
+
     private final ChatMessageService chatMessageService;
 
-    @MessageMapping("/chat/{roomId}")
-    @SendTo("/topic/messages/{roomId}")
-    public void sendMsg(@Payload ChatMessageDto message) {
-        chatMessageService.saveMessage(message);
+    @MessageMapping("/chat")
+    public void sendMsg(ChatMessageDto message) {
+        simpMessagingTemplate.convertAndSend("/topic/messages/" + message.getRoomId(), message);
+    }
+
+    @MessageMapping("/history/{roomId}")
+    public void getChatHistory(@DestinationVariable String roomId) {
+        List<ResponseChatMessageDto> chatHistory = chatMessageService.getChatHistory(roomId);
+        simpMessagingTemplate.convertAndSend("/topic/history/" + roomId, chatHistory);
     }
 
 }
