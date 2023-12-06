ea92806ee6310c3f2258d4685341a369d734e187
yesrin <asjs11@naver.com>
2023-12-06 14:07:47 +0900

    feat : 채팅 UI 구성 및 API 연결

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Preloader from './component/preloader/Preloader';
-import Nav from './component/nav/Nav';
-import Home from './component/main/home/Home';
-import Footer from './component/footer/Footer';
-import ArticleList from './component/article/list/ArticleList';
-import ArticleDetails from './component/article/detail/ArticleDetails';
-import SignIn from './component/user/signin';
-import SignUp from './component/user/signup';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Preloader from "./component/preloader/Preloader";
+import Nav from "./component/nav/Nav";
+import Home from "./component/main/home/Home";
+import Footer from "./component/footer/Footer";
+import ArticleList from "./component/article/list/ArticleList";
+import ArticleDetails from "./component/article/detail/ArticleDetails";
+import SignIn from "./component/user/signin";
+import SignUp from "./component/user/signup";
+import ChatMain from "./component/chat/ChatMain";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/donate" element={<ArticleList />} />
           <Route path="/donate/:articleId" element={<ArticleDetails />} />
+          <Route path="/chat" element={<ChatMain />} />
         </Route>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
