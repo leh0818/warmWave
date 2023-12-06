092a9bb8afc7f38293141577db8e4223444ef5a2
yunhuiGwak <kwakyh17@naver.com>
2023-12-06 15:33:55 +0900

    feat: User, Login, SignUp, Logout 컴포넌트 추가 App.js - Signup, Login, Logout 은 Nav 없이 화면구성

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,33 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Preloader from './component/preloader/Preloader';
 import Nav from './component/nav/Nav';
 import Home from './component/main/home/Home';
 import Footer from './component/footer/Footer';
 import ArticleList from './component/article/list/ArticleList';
 import ArticleDetails from './component/article/detail/ArticleDetails';
-import SignIn from './component/user/signin';
+import User from "./component/user/user";
+import Login from './component/user/login';
 import SignUp from './component/user/signup';
+import Logout from './component/user/logout'
+
+// import User from './component/user/user';
 
 function App() {
-  return (
-    <div>
-      <Preloader />
+    return (
+        <div>
+            <Preloader/>
 
-      <Routes>
-        <Route path="/" element={<Nav />}>
-          <Route index element={<Home />} />
-          <Route path="/donate" element={<ArticleList />} />
-          <Route path="/donate/:articleId" element={<ArticleDetails />} />
-        </Route>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-
-      <Footer />
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<Nav/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="/donate" element={<ArticleList/>}/>
+                    <Route path="/donate/:articleId" element={<ArticleDetails/>}/>
+                    <Route path="/user" element={<User/>}/>
+                </Route>
+                <Route path="/signup" element={<SignUp/>}/>
+                <Route path="/user/login" element={<Login/>}/>
+                <Route path="/user/logout" element={<Logout/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
