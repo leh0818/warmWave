3ce8ec0913613c1923a4fb9683d7ce0bfb6edac1
yunhuiGwak <kwakyh17@naver.com>
2023-12-14 15:02:54 +0900

    feat: authApi 회원가입,로그인,이메일인증 authApi.js - 유저관련 인증 api

---
+++ FE/src/component/user/signup&login/authApi.js
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import useToast from '../../hooks/useToast';
+import axios from 'axios';
+
+export const API_SERVER_HOST = 'http://localhost:8080';
+const host = API_SERVER_HOST;
+
+const instance = axios.create({
+    baseURL: host,
+    withCredentials: true,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
+});
+
+const useAuthAPI = () => {
+    const { showToast } = useToast();
+
+    const Login = async (auth) => {
+        try {
+            const response = await instance.post(`${host}/api/users/login`, auth);
+            return response;
+        } catch (error) {
+            showToast('에러가 발생했습니다');
+        }
+    };
+
+    const SignUp = async (auth) => {
+        try {
+            const response = await instance.post(`${host}/api/users/signup`, auth);
+            return response;
+        } catch (error) {
+            showToast('에러가 발생했습니다');
+        }
+    };
+
+    const emailValidCheck = async (email) => {
+        try {
+            const response = await instance.get(`${host}/api/users/signup/checkEmail/?email=${email}`);
+            return response;
+        } catch (error) {
+            showToast('에러가 발생했습니다');
+        }
+    };
+
+    return {
+        Login,
+        SignUp,
+        emailValidCheck,
+    };
+};
+
+export default useAuthAPI;
