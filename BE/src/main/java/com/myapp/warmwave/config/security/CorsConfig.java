b5a2cd42e3598a1ddda66e6222976dfc8230fdff
havegrit <79460763+havegrit@users.noreply.github.com>
2023-12-14 09:31:50 +0900

    Fix: CORS 관련하여 발생하던 리디렉션 문제 해결 및 CorsConfig 리팩토링 진행 (#104)

--- BE/src/main/java/com/myapp/warmwave/config/security/CorsConfig.java
+++ BE/src/main/java/com/myapp/warmwave/config/security/CorsConfig.java
@@ -3,22 +3,22 @@ package com.myapp.warmwave.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/api/**", config);
-
-        return new CorsFilter(source);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
--- BE/src/main/java/com/myapp/warmwave/config/security/SecurityConfig.java
+++ BE/src/main/java/com/myapp/warmwave/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.myapp.warmwave.config.security;
 import com.myapp.warmwave.common.jwt.JwtAuthFilter;
 import com.myapp.warmwave.config.oauth.OAuth2LoginFailureHandler;
 import com.myapp.warmwave.config.oauth.OAuth2LoginSuccessHandler;
+import com.myapp.warmwave.config.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,12 +23,22 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(STATELESS)
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/api/users/logout")
+                )
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilter(corsConfig.corsFilter())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
@@ -44,15 +55,9 @@ public class SecurityConfig {
                                 .loginPage("http://localhost:3000/user/login")
                                 .successHandler(oAuth2LoginSuccessHandler)
                                 .failureHandler(oAuth2LoginFailureHandler)
-                )
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(STATELESS)
-                )
-                .logout(
-                        logout -> logout
-                                .logoutUrl("/api/users/logout")
+                                .userInfoEndpoint(userInfo -> userInfo
+                                        .userService(customOAuth2UserService)
+                                )
                 );
 
         return http.build();
