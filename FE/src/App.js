1911a04b182f2563200237f351a93de76332d156
yunhuiGwak <kwakyh17@naver.com>
2023-12-14 10:03:30 +0900

    feat: App.js 회원가입(개인,기관) uri 추가 App.js - 회원가입 uri 분리 - 회원가입 페이지에서 개인,기관회원가입 페이지로 이동하도록 uri 분리

--- FE/src/App.js
+++ FE/src/App.js
@@ -10,11 +10,12 @@ import Footer from './component/footer/Footer';
 import ArticleList from './component/article/list/ArticleList';
 import ArticleDetails from './component/article/detail/ArticleDetails';
 import User from "./component/user/user";
-import Login from './component/user/login';
+import Login from './component/user/signup&login/login';
 import PostButton from './component/article/post/PostButton';
 import PostForm from './component/article/post/PostForm'; // PostForm을 import 합니다.
-import PatchForm from './component/article/post/PatchForm'; // PatchForm을 import 합니다.
-import SignUp from './component/user/signup';
+import SignUp from './component/user/signup&login/signup'
+import Individual_signup from './component/user/signup&login/individual_signup';
+import Institution_signup from './component/user/signup&login/institution_signup'
 import MyPage from './component/user/mypage/mypage';
 import CommunityList from './component/community/list/CommunityList';
 import CommunityDetails from './component/community/detail/CommunityDetails';
@@ -36,12 +37,15 @@ function App() {
                     <Route index element={<Home />} />
                     <Route path="/donate" element={<ArticleList />} />
                     <Route path="/donate/:articleId" element={<ArticleDetails />} />
-                    <Route path="/write" element={<PostForm />} />
-                    <Route path="/update/:articleId" element={<PatchForm />} /> 
+                    <Route path="/write" element={<PostForm />} /> {/* PostForm을 /write 경로에 연결합니다. */}
                     <Route path="/user" element={<User />} />
                     <Route path='/user/me' element={<MyPage />} />
                     <Route path="/community" element={<CommunityList />} />
                     <Route path="/community/:communityId" element={<CommunityDetails />} />
+                    <Route path="/signup" element={<SignUp />} />
+                </Route>
+                <Route path="/signup/individual" element={<Individual_signup />} />
+                <Route path="/signup/institution" element={<Institution_signup />} />
                     <Route path="/comment/:communityId" element={<Comment />} />
                     <Route path="/community/write" element={<CommunityWrite />} />
                     <Route path="/community/update/:communityId" element={<CommunityUpdate />} />
@@ -50,7 +54,7 @@ function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/user/login" element={<Login />} />
             </Routes>
-            {location.pathname !== '/signup' && location.pathname !== '/user/login' && <Footer />}
+            {location.pathname !== '/signup/individual' && location.pathname !== '/signup/institution' && location.pathname !== '/user/login' && <Footer />}
             {loginState.id && <PostButton />}
         </div>
     );
