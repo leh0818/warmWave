9cbb526e01719c863dab6a1ce38fc506bd7062f8
havegrit <shin_jaean@naver.com>
2023-12-18 11:01:46 +0900

    Feat: 전달 받은 데이터를 하위 컴포넌트에 전달하도록 작업

--- FE/src/component/user/mypage/myInfo.js
+++ FE/src/component/user/mypage/myInfo.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import EditMyInfo from './editIndivInfo';
+import React, { useState } from 'react';
 import ShowIndivInfo from './showIndivInfo';
 import ShowInstInfo from './showInstInfo';
 import EditInstInfo from './editInstInfo';
 import EditIndivInfo from './editIndivInfo';
+export const API_SERVER_HOST = 'http://localhost:8080'
 
-function MyInfo(props) {
-  const userType = props.userType;
-  const [info, setInfo] = useState([]);
+function MyInfo({ userType, userInfo }) {
   //status: 게시글이 정보 보여주기 상태인지 수정 가능 상태인지
-  const [status, setStatus] = useState("show");
+  const [status, setStatus] = useState('show');
+
+  if (!userInfo) {
+    return <div>Loading...</div>;
+  }
 
   const switchPage = status => {
     setStatus(status);
   }
 
-  if (userType == 'institution' && status == 'show') {
-    return <ShowInstInfo sendDataToParent={switchPage} />
-  } else if (userType == 'individual' && status == 'show') {
-    return <ShowIndivInfo sendDataToParent={switchPage} />
-  } else if (userType == 'institution' && status == 'edit') {
-    return <EditInstInfo sendDataToParent={switchPage} />
+  if (userType === 'institution' && status === 'show') {
+    return <ShowInstInfo userInfo={userInfo} sendDataToParent={switchPage} />
+  } else if (userType === 'individual' && status === 'show') {
+    return <ShowIndivInfo userInfo={userInfo} sendDataToParent={switchPage} />
+  } else if (userType === 'institution' && status === 'edit') {
+    return <EditInstInfo userInfo={userInfo} sendDataToParent={switchPage} />
   } else {
-    return <EditIndivInfo sendDataToParent={switchPage} />
+    return <EditIndivInfo userInfo={userInfo} sendDataToParent={switchPage} />
   }
 }
 
