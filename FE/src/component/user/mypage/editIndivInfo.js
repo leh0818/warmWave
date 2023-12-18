23ae61dafcd9f00c4ec21f09c9665c1f63fbfa6a
havegrit <shin_jaean@naver.com>
2023-12-18 11:07:41 +0900

    Feat: 내정보, 수정 페이지에서 사용자 데이터 보여주기

--- FE/src/component/user/mypage/editIndivInfo.js
+++ FE/src/component/user/mypage/editIndivInfo.js
@@ -1,49 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import jwtAxios from '../../util/jwtUtil';
+export const API_SERVER_HOST = 'http://localhost:8080'
 
-function EditIndivInfo({ sendDataToParent }) {
+function EditIndivInfo(props) {
+  const userInfo = props.userInfo;
+  const userId = userInfo.userId;
+  const userType = userInfo.userType;
+  const name = userInfo.name;
+  const email = userInfo.email;
+  const address = userInfo.address;
+  const [password, setPassword] = useState();
+
   const save = () => {
-
+    // console.log(`${API_SERVER_HOST}/api/users/${userId}/${userType}` + ' ' + 'back end api is calling !!!');
+    // jwtAxios.put(`${API_SERVER_HOST}/api/users/${userId}/${userType}`, editForm)
+    //   .then(res => {
+    //     const resData = res.data;
+    //     console.log(`${API_SERVER_HOST}/api/users/${userId}/${userType}` + ' ' + 'back end api is called successfully !!!');
+    //     console.log('response data: ' + JSON.stringify(resData));
+    //   });
   }
+
+  useEffect(() => {
+    const editForm = {
+      password: '',
+      nickname: name,
+      fullAddr: '',
+      sdName: '',
+      sggName: '',
+      details: ''
+    }
+
+
+  }, [])
+
   const goToInfoPage = () => {
-    sendDataToParent('show');
+    props.sendDataToParent('show');
   }
 
   return (
     <div className="col-md-8 mt-5">
-      <div className="card mb-3">
+      <div className="card">
         <div className="card-body">
           <div className="row">
-            <div className="col-sm-3">
+            <div className="col-sm-2">
               <h6 className="mb-0">닉네임</h6>
             </div>
-            <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+            <div className="col-sm-9">
+              <input className='form-control form-control-sm' type='text' defaultValue={name} />
             </div>
           </div>
-          <hr />
-          <div className="row">
-            <div className="col-sm-3">
+          <div className="row mt-1 mb-1">
+            <div className="col-sm-2">
               <h6 className="mb-0">이메일</h6>
             </div>
-            <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+            <div className="col-sm-9">
+              <input className='form-control form-control-sm' type='text' defaultValue={email} />
             </div>
           </div>
-          <hr />
           <div className="row">
-            <div className="col-sm-3">
+            <div className="col-sm-2">
               <h6 className="mb-0">주소</h6>
             </div>
-            <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+            <div className="col-sm-7">
+              <input className='form-control form-control-sm' type='text' defaultValue={address} />
+            </div>
+            <div className="col-sm-2">
+              <button>주소검색</button>
             </div>
           </div>
-          <hr />
-          <div className="row">
-            <div className="col-sm-12">
-              <a className="btn btn-info" onClick={save}>저장</a>
-              <a className="btn btn-secondary" onClick={goToInfoPage}>취소</a>
+          <div className="row mt-2">
+            <div className="col-sm-2">
+              <button className="btn btn-info w-100" onClick={save}>저장</button>
+            </div>
+            <div className="col-sm-2">
+              <button className="btn btn-secondary w-100" onClick={goToInfoPage}>취소</button>
             </div>
           </div>
         </div>
--- FE/src/component/user/mypage/editInstInfo.js
+++ FE/src/component/user/mypage/editInstInfo.js
@@ -1,59 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function EditInstInfo({ sendDataToParent }) {
+function EditInstInfo(props) {
+  const { name, email, address } = props.userInfo;
 
   const save = () => {
 
   }
+
   const goToInfoPage = () => {
-    sendDataToParent('show');
+    props.sendDataToParent('show');
   }
 
   return (
     <div className="col-md-8 mt-5">
-      <div className="card mb-3">
+      <div className="card">
         <div className="card-body">
           <div className="row">
             <div className="col-sm-3">
               <h6 className="mb-0">기관이름</h6>
             </div>
             <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+              <input className='form-control fomr-control-sm' type='text' defaultValue={name} />
             </div>
           </div>
-          <hr />
           <div className="row">
             <div className="col-sm-3">
               <h6 className="mb-0">이메일</h6>
             </div>
             <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+              <input className='form-control fomr-control-sm' type='text' defaultValue={email} />
             </div>
           </div>
-          <hr />
-          <div className="row">
-            <div className="col-sm-3">
-              <h6 className="mb-0">사업자번호</h6>
-            </div>
-            <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
-            </div>
-          </div>
-          <hr />
           <div className="row">
             <div className="col-sm-3">
               <h6 className="mb-0">기관주소</h6>
             </div>
             <div className="col-sm-9 text-secondary">
-              <input type='text' value="기존값 불러오기"></input>
+              <input className='form-control fomr-control-sm' type='text' defaultValue={address} />
             </div>
           </div>
-          <hr />
           <div className="row">
             <div className="col-sm-12">
-              <a className="btn btn-info" onClick={save}>저장</a>
-              <a className="btn btn-secondary" onClick={goToInfoPage}>취소</a>
+              <button className="btn btn-info" onClick={save}>저장</button>
+              <button className="btn btn-secondary" onClick={goToInfoPage}>취소</button>
             </div>
           </div>
         </div>
