d97c46bb1c41ff807929a598eda094689c49d4e2
EunHo <yyh8087@gmail.com>
2023-12-19 16:02:49 +0900

    feat : 게시글, 댓글 요청 유효성 검사 추가

--- BE/src/main/java/com/myapp/warmwave/domain/comment/dto/CommentRequestDto.java
+++ BE/src/main/java/com/myapp/warmwave/domain/comment/dto/CommentRequestDto.java
@@ -1,5 +1,7 @@
 package com.myapp.warmwave.domain.comment.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequestDto {
+    @NotNull
+    @Size(min = 10, max = 500, message = "댓글은 10자 이상 1000자 이하로 입력해야 합니다.")
     private String contents;
 }
