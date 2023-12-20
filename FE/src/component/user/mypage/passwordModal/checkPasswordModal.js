da3099a8f4e59fd8927ae695ebe2b86c60a9714f
havegrit <shin_jaean@naver.com>
2023-12-20 14:31:27 +0900

    Feat: 사용자 비밀번호 체크할 수 있는 모달 도입

---
+++ FE/src/component/user/mypage/passwordModal/checkPasswordModal.js
@@ -0,0 +1,63 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { getCookie } from "../../../util/cookieUtil";
+import axios from "axios";
+import { API_SERVER_HOST } from "../../../util/jwtUtil";
+
+function CheckPasswordModal(props) {
+  // const userInfo = props.userInfo;
+  const { email } = getCookie('user');
+
+  const handleClose = () => props.setShow(false);
+  const handleSubmit = e => {
+    const password = document.getElementById('password').value;
+    const loginData = {
+      email: email,
+      password: password
+    }
+    axios.post(`${API_SERVER_HOST}/api/users/login`, loginData)
+      .then(res => {
+        if (res.status === 200) {
+          //TODO: toast 성공 메시지 출력
+          props.setShow(false);
+          props.passwordCheckResult(true);
+        }
+
+      })
+      .catch((err) => {
+        const res = err.response;
+
+        if (res.status === 400) {
+          alert(res.data.msg);
+          //toast 에러 메시지 출력
+        }
+      });
+
+  }
+
+  return (
+    <div>
+      <Modal
+        show={props.show}
+        onHide={handleClose}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <div>비밀번호를 입력해주세요</div>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="password" id="password" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>
+            확인
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default CheckPasswordModal;
