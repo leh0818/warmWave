847617e250b8554d6d2cad133cae417d0b50ab10
EunHo <yyh8087@gmail.com>
2023-12-20 21:30:31 +0900

    fix : 댓글 유효성 검사 변경

--- BE/src/main/java/com/myapp/warmwave/domain/community/dto/CommunityPatchDto.java
+++ BE/src/main/java/com/myapp/warmwave/domain/community/dto/CommunityPatchDto.java
@@ -14,14 +14,11 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommunityPatchDto {
-    @NotNull
     @Size(min = 5, max = 50, message = "제목은 5자 이상 50자 이하로 입력해야 합니다.")
     private String title;
 
-    @NotNull
     @Size(min = 10, max = 1000, message = "내용은 10자 이상 1000자 이하로 입력해야 합니다.")
     private String contents;
 
-    @NotNull
     private String category;
 }
