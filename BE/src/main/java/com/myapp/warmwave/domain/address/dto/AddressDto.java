8b1c49aa19f12a2346c067bc84dec30fffab2923
havegrit <shin_jaean@naver.com>
2023-12-20 15:57:16 +0900

    Refactor: 주소 변경 없을 시 원래 객체 반환

--- BE/src/main/java/com/myapp/warmwave/domain/address/dto/AddressDto.java
+++ BE/src/main/java/com/myapp/warmwave/domain/address/dto/AddressDto.java
@@ -1,4 +1,42 @@
 package com.myapp.warmwave.domain.address.dto;
 
+import com.myapp.warmwave.domain.address.entity.Address;
+import com.myapp.warmwave.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class AddressDto {
+    public AddressDto(Address address) {
+        this.fullAddr = address.getFullAddr();
+        this.sdName = address.getSdName();
+        this.sggName = address.getSggName();
+        this.details = address.getDetails();
+    }
+
+    // 전체 주소
+    private String fullAddr;
+
+    // 시,도
+    private String sdName;
+
+    // 시,군,구
+    private String sggName;
+
+    // 상세주소
+    private String details;
+
+    public AddressDto extractAddressFromUser(User user) {
+        Address address = user.getAddress();
+        return new AddressDto(
+                address.getFullAddr(),
+                address.getSdName(),
+                address.getSggName(),
+                address.getDetails()
+        );
+    }
 }
