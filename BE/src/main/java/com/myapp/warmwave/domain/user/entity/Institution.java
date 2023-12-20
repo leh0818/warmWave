0dafbbb1d0854563646cecf70f0286c42f885824
yunhuiGwak <kwakyh17@naver.com>
2023-12-21 01:29:27 +0900

    feat: 기관 Entity에 사업자등록증이미지 컬럼 추가 Institution.java - 기관 회원가입 시 사업자등록증 업로드용 이미지 컬럼 - 이미지 엔티티와 1:1 매핑

--- BE/src/main/java/com/myapp/warmwave/domain/user/entity/Institution.java
+++ BE/src/main/java/com/myapp/warmwave/domain/user/entity/Institution.java
@@ -1,5 +1,7 @@
 package com.myapp.warmwave.domain.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.myapp.warmwave.domain.image.entity.Image;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -21,6 +23,12 @@ public class Institution extends User {
 
     private Boolean isApprove;
 
+    @OneToOne(mappedBy = "institution",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Image institutionImage;    // 사업자등록증 image
+
     public void approve() {
         this.isApprove = true;
     }
@@ -28,5 +36,9 @@ public class Institution extends User {
     @Override
     public String getName() {
         return this.institutionName;
+    }
+
+    public void addInstitutionImage(Image institutionImage) {
+        this.institutionImage = institutionImage;
     }
 }
