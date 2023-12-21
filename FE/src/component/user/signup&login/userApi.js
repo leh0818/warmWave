8027a39c27b665a09687c4a9d382613365a4b3bf
havegrit <shin_jaean@naver.com>
2023-12-21 15:38:33 +0900

    Refactor: withCredentials 옵션 제거

--- FE/src/component/user/signup&login/userApi.js
+++ FE/src/component/user/signup&login/userApi.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-
 const host = process.env.REACT_APP_HOST
 
 export const loginPost = async (loginParam) => {   // 사용자가 로그인을 시도할 때 호출
@@ -10,7 +9,7 @@ export const loginPost = async (loginParam) => {   // 사용자가 로그인을 
         password: loginParam.password
     }
 
-    const res = await axios.post(`${host}/api/users/login`, loginData, { withCredentials: true }) //  'axios.post'를 사용하여 서버에 로그인 요청 전송, 첫 번째 인자는 요청 URL, 두 번째 인자는 요청 본문, 세 번째 인자는 요청 config
-
-    return res.data // 서버로부터 받은 응답의 데이터를 반환
+    //  'axios.post'를 사용하여 서버에 로그인 요청 전송, 첫 번째 인자는 요청 URL, 두 번째 인자는 요청 본문, 세 번째 인자는 요청 config
+    const res = await axios.post(`${host}/api/users/login`, loginData)
+    return res.data
 }
