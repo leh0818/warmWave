a8d37c2e8ca05c5109828da36b2fc27be6f8b426
havegrit <shin_jaean@naver.com>
2023-12-21 15:43:26 +0900

    Feat: 수정 페이지 디자인 및 수정 데이터 백엔드 api 호출 시 전달하도록 작업

--- FE/src/component/user/mypage/editIndivInfo.js
+++ FE/src/component/user/mypage/editIndivInfo.js
@@ -1,34 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import KakaoPost from '../kakaopost/KakaoPost.js'
+import React, { useEffect, useState } from 'react';
+import jwtAxios, { API_SERVER_HOST } from '../../util/jwtUtil.js';
+import DaumPost from '../daumPost.js';
 import ChangePasswordModal from './passwordModal/changePasswordModal.js';
 
 function EditIndivInfo(props) {
   const userInfo = props.userInfo;
-  const { name, email, address } = userInfo;
+  const { userId, userType, name, email, address } = userInfo;
   const [changeModalShow, setChangeModalShow] = useState(false);
+  const [editData, setEditData] = useState({});
 
   const handleChangeModalShow = () => {
     setChangeModalShow(true);
   }
 
   const handleSaveChangedInfo = () => {
-    const editForm = {
+    setEditData(prevData => ({
+      ...prevData,
+      additionalProperty: { nickname: document.querySelector("#nickname").value }
+    }));
 
-    }
-    // console.log(`${API_SERVER_HOST}/api/users/${userId}/${userType}` + ' ' + 'back end api is calling !!!');
-    // jwtAxios.put(`${API_SERVER_HOST}/api/users/${userId}/${userType}`, editForm)
-    //   .then(res => {
-    //     const resData = res.data;
-    //     console.log(`${API_SERVER_HOST}/api/users/${userId}/${userType}` + ' ' + 'back end api is called successfully !!!');
-    //     console.log('response data: ' + JSON.stringify(resData));
-    //   });
+    jwtAxios.put(`${API_SERVER_HOST}/api/users/${userId}/${userType}`, editData)
+      .then(res => {
+        const resData = res.data;
+      })
+      .catch(err => {
+
+      });
   }
 
   useEffect(() => {
 
   }, [])
 
-  const handleSubmitKakaoPost = (localAddress, fullAddr) => {
+  const handleSubmitKakaoPost = (address) => {
+    const sidoAddress = address.provinceAddress;
+    const sigunguAddress = address.cityAddress;
+    const detailAddress = address.townAddress;
+    const fullAddress = address.provinceAddress + address.cityAddress + address.townAddress;
+
+    setEditData({
+      sdName: sidoAddress,
+      sggName: sigunguAddress,
+      details: detailAddress,
+      fullAddr: fullAddress
+    });
+
+    const inputAddress = document.querySelector('#address');
+    inputAddress.value = fullAddress;
   }
 
   const goToInfoPage = () => {
@@ -43,34 +61,34 @@ function EditIndivInfo(props) {
             <div className="d-flex col-sm-2 align-items-center">
               <h6 className="mb-0">닉네임</h6>
             </div>
-            <div className="col-sm-9">
-              <input className='form-control form-control-sm' type='text' defaultValue={name} />
+            <div className="d-flex col-sm-9 align-items-center">
+              <input id='nickname' className='form-control form-control-sm' type='text' defaultValue={name} />
             </div>
           </div>
-          <div className="row mt-1 mb-1">
+          <div className="row mt-1">
             <div className="d-flex col-sm-2 align-items-center">
               <h6 className="mb-0">이메일</h6>
             </div>
-            <div className="col-sm-9">
+            <div className="d-flex col-sm-9 align-items-center">
               <input className='form-control form-control-sm' type='text' defaultValue={email} />
             </div>
           </div>
-          <div className="row">
+          <div className="row flex-nowrap">
             <div className="d-flex col-sm-2 align-items-center">
               <h6 className="mb-0">주소</h6>
             </div>
-            <div className="col-sm-7">
+            <div className="d-flex col-sm-7 align-items-center">
               <input id='address' className='form-control form-control-sm' type='text' defaultValue={address} />
             </div>
-            <div className="col-sm-2">
-              <KakaoPost callFunction={handleSubmitKakaoPost} />
+            <div className="d-flex align-items-center col-sm-2">
+              <DaumPost callFunction={handleSubmitKakaoPost} />
             </div>
           </div>
           <div className="row">
             <div className="d-flex col-sm-2 align-items-center">
               <h6 className="mb-0">비밀번호</h6>
             </div>
-            <div className="col-sm-7">
+            <div className="d-flex col-sm-7 align-items-center">
               <button onClick={handleChangeModalShow}>비밀번호 변경</button>
             </div>
           </div>
